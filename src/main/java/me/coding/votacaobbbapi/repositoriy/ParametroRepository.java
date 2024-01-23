fa9db6fb49ccc91fb64a5d3a68aa8db7cc298c10
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-01-23 19:52:12 -0300

    feat: create param repository

---
+++ src/main/java/me/coding/votacaobbbapi/repositoriy/ParametroRepository.java
@@ -0,0 +1,9 @@
+package me.coding.votacaobbbapi.repositoriy;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import me.coding.votacaobbbapi.model.ParametroModel;
+
+public interface ParametroRepository extends MongoRepository<ParametroModel, String>{
+
+}
