809bf2ed9bf30039b8b99361d330d37b610256b8
devmarcosav@gmail.com <devmarcosav@gmail.com>
2024-01-23 18:29:39 -0300

    feat: create con kafka and api rest

---
+++ src/main/java/me/coding/votacaobbbapi/controller/StatusController.java
@@ -0,0 +1,13 @@
+package me.coding.votacaobbbapi.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+    @GetMapping("/api/status")
+    public ResponseEntity<String> status() {
+        return ResponseEntity.ok("online");
+    }
+}
